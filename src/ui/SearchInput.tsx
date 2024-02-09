06c744abb310e0655986445cb551b4d24a06575b
MakoDoli <makodolidze@hotmail.com>
2024-02-09 14:53:03 +0400

    last commit

--- src/ui/SearchInput.tsx
+++ src/ui/SearchInput.tsx
@@ -3,8 +3,10 @@ import { ChangeEvent } from "react";
 import { SearchProps } from "@/types/interface";
 
 export default function SearchInput({ setUser }: SearchProps) {
+  let timeoutId: number;
   function handleChange(e: ChangeEvent<HTMLInputElement>) {
-    window.setTimeout(() => {
+    clearTimeout(timeoutId);
+    timeoutId = window.setTimeout(() => {
       setUser(e.target.value);
     }, 2000);
   }
