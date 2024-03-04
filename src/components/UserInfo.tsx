7c185d6c96f6ad11150cbb7acb6f764aa0e1ecc4
MakoDoli <makodolidze@hotmail.com>
2024-03-04 21:32:48 +0400

    add aceternity/ui

--- src/components/UserInfo.tsx
+++ src/components/UserInfo.tsx
@@ -4,14 +4,14 @@ import { UserType } from "@/types/interface";
 import { BASE_URL_USER } from "@/utils/constants";
 import { Link } from "@tanstack/react-router";
 import { useEffect, useState } from "react";
-import {SearchType} from '../types/interface'
+import { SearchType } from "../types/interface";
 import {
   HoverCard,
   HoverCardContent,
   HoverCardTrigger,
 } from "@/components/ui/hover-card";
 import github from "../assets/github.png";
-
+import { BackgroundGradient } from "./ui/background-gradient";
 function UserInfo({ searchValue }: SearchType) {
   const postId = searchValue;
 
@@ -54,46 +54,48 @@ function UserInfo({ searchValue }: SearchType) {
   if (isLoading) return <Spinner />;
   return (
     <div className="w-full pt-12 h-screen dark:bg-slate-900">
-      <div className="w-[300px] md:w-[500px] border border-slate-300 dark:border-black shadow-md  rounded -2xl  mx-auto text-center p-2 dark:bg-slate-800">
-        <HoverCard>
-          <HoverCardTrigger asChild>
-            <img
-              className="rounded-full w-24 mx-auto mt-6 cursor-pointer"
-              src={avatarUrl}
-            />
-          </HoverCardTrigger>
-          <HoverCardContent className="w-80">
-            <div className="flex justify-between space-x-4">
-              <a href={htmlUrl} target="_blank" rel="noreferrer">
-                <img src={github} />
-                <div className="space-y-1">
-                  <h4 className="text-sm text-teal-500 font-semibold">
-                    Click here for
-                  </h4>
-                  <p className="text-sm"> {login}'s github account</p>
-                </div>
-              </a>
-            </div>
-          </HoverCardContent>
-        </HoverCard>
+      <div className="w-[300px] md:w-[500px] border border-slate-300 dark:border-black shadow-md  rounded-2xl  mx-auto text-center p-2 dark:bg-slate-800">
+        <BackgroundGradient className="rounded-[22px] max-w-[500px] p-4 sm:p-6 bg-white dark:bg-zinc-900 items-center">
+          <HoverCard>
+            <HoverCardTrigger asChild>
+              <img
+                className="rounded-full w-24 mx-auto md:mt-6 cursor-pointer"
+                src={avatarUrl}
+              />
+            </HoverCardTrigger>
+            <HoverCardContent className="w-80">
+              <div className="flex justify-between space-x-4">
+                <a href={htmlUrl} target="_blank" rel="noreferrer">
+                  <img src={github} />
+                  <div className="space-y-1">
+                    <h4 className="text-sm text-teal-500 font-semibold">
+                      Click here for
+                    </h4>
+                    <p className="text-sm"> {login}'s github account</p>
+                  </div>
+                </a>
+              </div>
+            </HoverCardContent>
+          </HoverCard>
 
-        <div className="flex m-10 flex-wrap ">
-          <div className="w-1/2 dark:text-white">
-            <p>Login: </p>
-            <p>Followers: </p>
-            <p> Public repos: </p>
-            <p>Bio: </p>
+          <div className="flex m-10 flex-wrap ">
+            <div className="w-1/2 dark:text-white">
+              <p>Login: </p>
+              <p>Followers: </p>
+              <p> Public repos: </p>
+              <p>Bio: </p>
+            </div>
+            <div className="text-teal-500 w-1/2">
+              <p>{login}</p>
+              <p>{followers}</p>
+              <p>{publicRepos}</p>
+              <p>{bio}</p>
+            </div>
           </div>
-          <div className="text-teal-500 w-1/2">
-            <p>{login}</p>
-            <p>{followers}</p>
-            <p>{publicRepos}</p>
-            <p>{bio}</p>
-          </div>
-        </div>
-        <Button>
-          <Link to="/">Back</Link>
-        </Button>
+          <Button>
+            <Link to="/">Back</Link>
+          </Button>
+        </BackgroundGradient>
       </div>
     </div>
   );
